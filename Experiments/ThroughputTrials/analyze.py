a37cff91bb43518af34655b7bba22a41a01ee6c4
saahilclaypool <saahil@claypools.org>
2020-05-17 16:29:43 -0400

    Add feather format

--- Experiments/ThroughputTrials/analyze.py
+++ Experiments/ThroughputTrials/analyze.py
@@ -9,6 +9,7 @@ from glob import glob
 from os import path
 import numpy as np
 import os
+import feather
 import pandas as pd
 
 
@@ -54,6 +55,7 @@ def pcap_to_csv(pcap_file='./data/pcap.pcap', reparse=True):
         -e tcp.srcport \
         -e tcp.dstport \
         -e tcp.seq \
+        -e tcp.analysis.ack_rtt \
         -e ip.proto  \
         -e frame.time \
         -E header=y  \
@@ -78,22 +80,59 @@ def select_data_flow(groups):
             max_group = group
     return max_group
 
+def update_dataframe(df, filename):
+    base, _ext = path.splitext(filename)
+    feather_file = base + '.feather'
+    feather.write_dataframe(df, feather_file)
+    return df
+
+def load_dataframe(filename):
+    """
+    opens csvfile and writes out .feather file
+
+    return: dataframe
+    """
+    base, _ext = path.splitext(filename)
+    feather_file = base + '.feather'
+    if (path.isfile(feather_file)):
+        return feather.read_dataframe(feather_file)
+    else:
+        df = pd.read_csv(filename)
+        df['time'] = pd.to_datetime(df['frame.time'], infer_datetime_format=True)
+        feather.write_dataframe(df, feather_file)
+        return df
+
+def parsed_filenames(filename):
+    base, _ext = path.splitext(filename)
+    receiver_path = base + '_receiver.feather'
+    sender_path = base + '_sender.feather'
+    return sender_path, receiver_path
 
 def parse_csv(filename):
     """
     return pandas version of the csv
     """
-    df = pd.read_csv(filename).dropna()
-    df['time'] = pd.to_datetime(df['frame.time'])
+    sender_path, receiver_path = parsed_filenames(filename)
+
+    if (path.isfile(receiver_path) and path.isfile(sender_path)):
+        receiver_flow = feather.read_dataframe(receiver_path)
+        sender_flow = feather.read_dataframe(sender_path)
+        return (sender_flow, receiver_flow)
+
+
+    df = load_dataframe(filename).dropna()
     df = df.set_index('frame.time').sort_index()
 
     group_tuple = ["ip.src", "ip.dst", "tcp.srcport", "tcp.dstport"]
 
     sender_traffic = df[df['ip.src'] != LOCAL].groupby(group_tuple)
     sender_flow = select_data_flow(sender_traffic)
+    feather.write_dataframe(sender_flow, sender_path)
 
     receiver = df[df['ip.src'] == RECEIVER].groupby(group_tuple)
     receiver_flow = select_data_flow(receiver)
+    feather.write_dataframe(receiver_flow, receiver_path)
+
     return (sender_flow, receiver_flow)
 
 def parse_directory(directory):
@@ -161,7 +200,6 @@ def analyze(local, remote, dir):
     print(local, remote)
     local_csv = pcap_to_csv(local, False)
     local_sender_flow, local_receiver_flow = parse_csv(local_csv)
-    print('parsed csv')
     # remote_sender_flow, remote_receiver_flow = parse_csv(local_csv)
     return [summary(local_sender_flow, dir),
             # summary(local_receiver_flow, dir),
@@ -184,6 +222,35 @@ def main():
     df = pd.DataFrame(throughputs)
     df.to_csv(f"{DATA_DIR}/quantiles.csv")
 
+
+def timeslice(filename):
+    """
+    determine the time it takes to 
+    """
+    dirname = path.dirname(filename)
+    sender_path, receiver_path = parsed_filenames(filename)
+    sender = feather.read_dataframe(sender_path)
+    gig = 1e+9
+    num_objects = 100
+    times = []
+    base_time = sender.time.min()
+
+    for i in range(num_objects):
+        bytes_to_download = (i + 1) * (gig / num_objects)
+        time_to_download = sender[sender['tcp.seq'] > bytes_to_download].iloc[0].time - base_time
+        times.append({ 'file_size': bytes_to_download, 'time': time_to_download })
+
+    df = pd.DataFrame(times)
+    df.to_feather(dirname + '/timeslice.feather')
+
+    plt.close()
+    plt.plot(df.file_size / gig, df.time * 1e-9)
+    plt.xlabel('Download gig')
+    plt.ylabel('Time (seconds)')
+    plt.savefig(dirname + '/slice.png')
+
+    print(times)
+
 def retrofit_times(directory):
     """
     doesn't give good data...
@@ -192,7 +259,7 @@ def retrofit_times(directory):
     start_times = []
     dirs = [dir for (_, _, dir) in all_pcaps()]
     csvfile = f'{directory}/quantiles.csv'
-    quantiles_df = pd.read_csv(csvfile)
+    quantiles_df = load_dataframe(csvfile)
     for d in dirs:
         data_df = pd.read_csv(f'{d}/local.csv', nrows=2)
         start_time = data_df['frame.time'][0]
